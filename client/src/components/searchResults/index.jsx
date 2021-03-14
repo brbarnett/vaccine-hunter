2f41697b771841397c0d9bda9c23966e652863df
Brandon Barnett <bbarnett@phnxsports.com>
2021-03-14 09:37:44 -0500

    Adding website links in

--- client/src/components/searchResults/index.jsx
+++ client/src/components/searchResults/index.jsx
@@ -1,3 +1,9 @@
+const website = {
+    "cvs": "https://www.cvs.com/immunizations/covid-19-vaccine",
+    "walgreens": "https://www.walgreens.com/findcare/vaccination/covid-19?ban=covid_vaccine_landing_schedule",
+    "walmart": "https://www.walmart.com/pharmacy/clinical-services/immunization/scheduled?imzType=covid",
+};
+
 const SearchResults = (props) => {
     const { isHunting, locationsWithAppointments } = props;
 
@@ -19,7 +25,8 @@ const SearchResults = (props) => {
                     <span className="badge badge-success m-2">{location.brand}</span>
                     {location.name}{' '}
                         - {location.address}, {location.city}, {location.state} {location.postal_code} ({location.distance} miles){' '}
-                        - {location.appointments.length > 0 ? location.appointments.length : `unknown`} appointments
+                        - {location.appointments.length > 0 ? location.appointments.length : `unknown`} appointments{' '}
+                    <a href={website[location.brand]} target="__blank">click here</a>
                 </p>
             ))}
         </div>
